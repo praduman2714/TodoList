6dc184c3abebd31b3b70bd8f20886baf13d914cf
praduman2714 <66240716+praduman2714@users.noreply.github.com>
2023-01-30 22:53:24 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,88 +1,97 @@
-const tasks = [];
+let tasks = [];
 
-const taskList = document.getElementById('list');
-const addTaskInput = document.getElementById('add');
-const tasksCounter = document.getElementById('tasks-counter');
+const tasksList = document.getElementById("list");
+const addTaskInput = document.getElementById("add");
+const taskCounter = document.getElementById("tasks-counter");
 
-// console.log('Working');
 
 function addToDom(task){
     const li = document.createElement('li');
     li.innerHTML = `
-        
-            <input type="checkbox" id="${task.id}" ${task.done ? 'checked' : ''}  class="custom-checkbox">
-            <label for="${task.id}"> ${task.text} </label>
-            <img src="dele.png" class="delete" data-id="${task.id}" />
-        
+        <input type="checkbox" id="${task.id}" ${task.completed ? 'checked' : ''} class="custom-checkbox">
+        <label for="${task.id}">${task.text}</label>
+        <img src="dele.png" class="delete" data-id="${task.id}" />
     `;
 
-    taskList.append(li);
+    tasksList.append(li);
 }
 
 
-function renderList () {
-    taskList.innerHTML = "";
-    for(var i = 0; i<tasks.length; i++){
+function renderList(){
+    tasksList.innerHTML = "";
+    for(let i = 0; i<tasks.length ; i++){
         addToDom(tasks[i]);
+
     }
-    tasksCounter.innerHTML = tasks.length;
+    taskCounter.innerHTML = tasks.length;
 }
 
-function markTaskAsComplete (taskId) {
-    const tasks = tasks.filter(function(task){
-        return task.id !== taskId 
-    });
-    if(tasks.length > 0){
-        const currentTask = task[0];
-        currentTask.done = true;
-        showNotification("Task Accomplised");
-        renderList();
-        return;
-    }
-}
-
-function deleteTask (taskId) {
-    const newTasks = tasks.filter(function(task){
-        return task.id !== taskId 
-    });
-    tasks = newTasks;
-    renderList();
-    showNotification("Tasks Deleted :)")
-
-}
-
-function addTask (task) {
+function addTasks(task){
     if(task){
         tasks.push(task);
+        showNotification("Plan Added")
         renderList();
-        showNotification("Task added sucessfully!");
         return;
     }
-    showNotification("Task can not be added :(");
+    showNotification("Task cannot be added");
 }
 
-function showNotification(text) {
-    alert(text);
-}
-
-function handleInput(event){
-    if(event.key === 'Enter'){
-        const text = event.target.value;
-
-        if(text === ""){
-            showNotification("The Input box is Empty!");
-            return;
+function toggleTasks(taskId){
+    for(let i = 0; i<tasks.length; i++){
+        if(tasks[i].id === taskId){
+            tasks[i].completed = !tasks[i].completed;
+            showNotification("Tasks Toggled");
         }
-        // addTask(text);
-        const task = {
-            text,
-            date : Date.now().toString(),
-            done : false,
-        }
-    
-        addTask(task);
-        event.target.value = "";
     }
 }
 
-addTaskInput.addEventListener("keyup" , handleInput);
+function deleteTasks(taskID){
+    const newTask = tasks.filter(function(task){
+        return task.id !== taskID;
+    })
+    tasks = newTask;
+    renderList();
+    showNotification("Task deleted");
+}
+
+function showNotification(task){
+    alert(task);
+    return ;
+}
+
+function handleInput(e){
+    if(e.key === 'Enter'){
+        const text = e.target.value;
+
+        if(!text){
+            showNotification("Tell me what is your plan !");
+            return;
+        }
+
+        const task = {
+            text,
+            id : Date.now().toString(),
+            completed : false,
+        };
+        e.target.value= "";
+        addTasks(task);
+    }
+}
+
+function handleClickListner(event){
+    const target = event.target;
+    // console.log(target);
+
+    if(target.className === 'delete'){
+        const id = target.dataset.id;
+        deleteTasks(id);
+        return ;
+    }else if(target.className === 'custom-checkbox'){
+        const id = target.id;
+        toggleTasks(id);
+        return;
+    }
+}
+
+addTaskInput.addEventListener('keyup', handleInput);
+document.addEventListener('click', handleClickListner);
